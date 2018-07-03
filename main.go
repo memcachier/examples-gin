be518411c7395889f33fbcc25a5967148c599b4b
Sascha <sascha.trifunovic@gmail.com>
2018-07-03 14:55:29 +0200

    Add caching

--- main.go
+++ main.go
@@ -6,9 +6,17 @@ import (
   "strconv"
 
   "github.com/gin-gonic/gin"
+  "github.com/memcachier/mc"
 )
 
 func main() {
+  username := os.Getenv("MEMCACHIER_USERNAME")
+  password := os.Getenv("MEMCACHIER_PASSWORD")
+  servers := os.Getenv("MEMCACHIER_SERVERS")
+
+  mcClient := mc.NewMC(servers, username, password)
+  defer mcClient.Quit()
+
   port := os.Getenv("PORT")
 
   if port == "" {
@@ -33,7 +41,19 @@ func main() {
           "error": "Please submit a valid number between 1 and 10000.",
         })
       } else {
-        p := calculatePrime(i)
+        key := "prime." + strconv.Itoa(i)
+        p := 0
+        // Look in cache
+        val, _, _, err := mcClient.Get(key)
+        if err != nil {
+          // Prime not in cache (calculate and store)
+          p = calculatePrime(i)
+          val = strconv.Itoa(p)
+          mcClient.Set(key, val, 0, 0, 0)
+        } else {
+          // Found it!
+          p, _ = strconv.Atoi(val)
+        }
         // Render view with prime
         c.HTML(http.StatusOK, "index.tmpl.html", gin.H{"n": i, "prime": p})
       }
