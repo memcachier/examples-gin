1a62f303fa47797d11e295032cd5ce180f247ce5
Lindsey Jacks <linzjax@gmail.com>
2018-07-25 16:26:22 -0400

    Add caching

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -xe
 
+# get all of the dependencies needed
 go get "github.com/gin-gonic/gin"
+go get "github.com/memcachier/mc"
 
+# create the application binary that eb uses
 GOOS=linux GOARCH=amd64 go build -o bin/application -ldflags="-s -w"
